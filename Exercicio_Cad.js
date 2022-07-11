f91af26666eb4b57fc597d1cba0f7dc8253be39e
MatheusSchiavao <matheus.schiavao08@gmail.com>
2022-07-11 12:00:26 -0300

    first commit

---
+++ Exercicio_Cad.js
@@ -0,0 +1,11 @@
+alert("Bem-vindo! A seguir pediremos que informe alguns dados.")
+
+let name = prompt("Qual seu nome, piloto?")
+
+let age = prompt("Qual a sua idade?")
+
+let ageConfirmation = confirm("Você tem " + age + " anos?")
+
+  alert("Nome: " + name)
+  alert("Idade: " + age)
+  alert("Idade confirmada: " + ageConfirmation)
